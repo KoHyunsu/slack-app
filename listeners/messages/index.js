2075e686b1231dfa11a0bc324e5a13a703ca9f1d
KoHyunsu <sce06147@gmail.com>
2023-10-10 12:48:45 +0900

    fix: 멘션 후 요청 질문내용 확인 메세지 추가

--- listeners/messages/index.js
+++ listeners/messages/index.js
@@ -2,4 +2,33 @@ const { sampleMessageCallback } = require('./sample-message');
 
 module.exports.register = (app) => {
   app.message(/^(hi|hello|hey).*/, sampleMessageCallback);
+  app.message('cat', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+  });
+  
+  app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+  });
 };
